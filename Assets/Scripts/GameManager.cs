fa75d539a01b2b446ef604751d83f21ca501976b
Thijs Moens <post@thijs.email>
2022-07-12 21:39:55 +0200

    📦 NEW: Setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TM
+{
+    public class GameManager : MonoBehaviour
+    {
+        public int maxHeight = 15;
+        public int maxWidth = 17;
+
+        public Color color1;
+        public Color color2;
+
+        GameObject mapObject;
+        SpriteRenderer mapRenderer;
+
+        private void Start()
+        {
+            CreateMap();
+        }
+
+        void CreateMap()
+        {
+            mapObject = new GameObject("Map");
+            mapRenderer = mapObject.AddComponent<SpriteRenderer>();
+
+            Texture2D txt = new Texture2D(maxWidth, maxHeight);
+            for (int x = 0; x < maxWidth; x++)
+            {
+                for (int y = 0; y < maxHeight; y++)
+                {
+                    #region Visual
+                    if (x % 2 != 0)
+                    {
+                        if (y % 2 != 0)
+                        {
+                            txt.SetPixel(x, y, color1);
+                        }
+                        else
+                        {
+                            txt.SetPixel(x, y, color2);
+                        }
+                    }
+                    else
+                    {
+                        if (y % 2 != 0)
+                        {
+                            txt.SetPixel(x, y, color2);
+                        }
+                        else
+                        {
+                            txt.SetPixel(x, y, color1);
+                        }
+                    }
+                    #endregion
+                }
+            }
+
+            txt.Apply();
+            Rect rect = new Rect(0,0, maxWidth, maxHeight);
+            Sprite sprite = Sprite.Create(txt, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
+            mapRenderer.sprite = sprite;
+        }
+    }
+}
